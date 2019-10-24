e83ffeecc3119af19e90bd1cfb05a7a94355ecdc
Taejoon Kim <tkim00@stuy.edu>
2019-10-24 17:36:28 -0400

    tested some stuff with sqlite to see whether I can inspect the tables through python alone; it didn't work for some reason

--- Team/db_builder.py
+++ Team/db_builder.py
@@ -7,19 +7,28 @@ db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
 c.execute('''CREATE TABLE IF NOT EXISTS USERNAMES(
             [id] INTEGER PRIMARY KEY, [username] text NOT NULL, [password] text);''')
+"""
 c.execute('''
     INSERT INTO USERNAMES VALUES(0, "username", "password");
 ''') #temporary
+"""
+foo = c.execute('''
+    SELECT [username] FROM USERNAMES;
+''')
+print(foo)
 
 c.execute('''CREATE TABLE IF NOT EXISTS STORIES
              ([id] INTEGER PRIMARY KEY, [title] text NOT NULL, [TEXT] text, [Date] text);''')
+"""
 c.execute('''
     INSERT INTO STORIES VALUES(2, "The Green Pancake", "blah blah blah", "2019-09-10");
 ''')
+"""
 
 '''
 foo = c.execute(q)
 print (foo)
 '''
+
 db.commit() #save changes
 db.close()  #close database
