4aaad8bc69aca1108d4d1ec95bd4fdbe26b58c98
Taejoon Kim <tkim00@stuy.edu>
2019-10-23 23:53:23 -0400

    worked further on db builder but have not created db file yet.

--- Team/db_builder.py
+++ Team/db_builder.py
@@ -5,8 +5,17 @@ import sqlite3   #enable control of an sqlite database
 DB_FILE="accounts.db"
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
-c.excute('''CREATE TABLE USERNAMES
-             ([id] INTEGER, [username] text, [password] text''')
+c.execute('''CREATE TABLE USERNAMES
+             ([id] INTEGER PRIMARY KEY, [username] text NOT NULL, [password] text);''')
+c.execute('''
+    INSERT INTO USERNAMES VALUES(num, usernam, passwor);
+''')
+
+c.execute('''CREATE TABLE STORIES
+             ([id] INTEGER PRIMARY KEY, [title] text NOT NULL, [TEXT] text, [Date] text);''')
+c.execute('''
+    INSERT INTO STORIES VALUES(2, "The Green Pancake", "blah blah blah", "2019-09-10");
+''')
 
 '''
 foo = c.execute(q)
