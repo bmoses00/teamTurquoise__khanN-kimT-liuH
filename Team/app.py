9935ea5ab56aa199b4946fd6f46ca2f3b691af1a
Henry Liu <hliu01@stuy.edu>
2019-10-27 16:39:37 -0400

    Added utl folder to store db functions

--- Team/app.py
+++ Team/app.py
@@ -5,6 +5,7 @@ from flask import redirect
 from flask import url_for
 from flask import flash
 from flask import session
+from utl import dbFunctions
 import sqlite3
 import os
 
@@ -12,7 +13,7 @@ import os
 DB_FILE="accounts.db"
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 #c = db.cursor()
-c = sqlite3.connect('accounts.db', check_same_thread=False).cursor()
+#c = sqlite3.connect('accounts.db', check_same_thread=False).cursor()
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
@@ -27,24 +28,12 @@ def root():
     if ("username" in request.args) & ("password" in request.args):
         username = request.args["username"]
         password = request.args["password"]
-        if (password == "1") & (username == "1"):
-            return redirect(url_for("signup"))
-        #If password and usernamee are correct, say so
-        if (password == "1234") & (username == "Peglegs"):
+        #If password and usernamee are correct
+        if (dbFunctions.accountExists(username,password) > -1):
             return redirect(url_for("success"))
-        #If password and usernamee are incorrect,
-        elif (username != "Peglegs") & (password != "1234"):
-            reason = " Your usernamee and password were both incorrect"
-            flash(reason)
-            return redirect(url_for("try_again"))
-        #If usernamee is incorrect, say so
-        elif (username != "Peglegs"):
-            reason = " Not valid usernamee"
-            flash(reason)
-            return redirect(url_for("try_again"))
-        #If password is incorrect, say so
+        #If credentials incorrect
         else:
-            reason = " Your password was incorrect"
+            reason = "invalid"
             flash(reason)
             return redirect(url_for("try_again"))
     return render_template(
@@ -59,20 +48,13 @@ def try_again():
     if ("username" in request.args) & ("password" in request.args):
             username = request.args["username"]
             password = request.args["password"]
-            #If password and usernamee are correct, say so
-            if (password == "1234") & (username == "Peglegs"):
+            #If password and usernamee are correct
+            if (dbFunctions.accountExists(username,password) > -1):
                 return redirect(url_for("success"))
-            #If password and usernamee are incorrect, say so
-            elif (username != "Peglegs") & (password != "1234"):
-                reason = " Your username and password were both incorrect"
-                return redirect(url_for("try_again"))
-            #If usernamee is incorrect, say so
-            elif (username != "Peglegs"):
-                reason = " Your username was incorrect"
-                return redirect(url_for("try_again"))
-            #If password is incorrect, say so
+            #If credentials incorrect
             else:
-                reason = " Your password was incorrect"
+                reason = "invalid"
+                flash(reason)
                 return redirect(url_for("try_again"))
     return render_template(
         'error.html',reasonforError=reason
@@ -81,23 +63,20 @@ def try_again():
 @app.route("/signup")
 def signup():
     global reason
-    #print(url_for("success"))
-    #if ("username" in request.args) & ("password" in request.args) & ("password2" in request.args):
-    #        password = request.args["password"]
-    #        password2 = request.args["password2"]
-    #        username = request.args["username"]
-    #        if (password == password2):
-    #            c.execute('''INSERT INTO USERNAMES VALUES("1", "0", "0");''')
-    c.execute('INSERT INTO USERNAMES VALUES (?, ?, ?)',("2", "pppppp", "e"))
-    return redirect("/")
+    if ("username" in request.args) & ("password" in request.args) & ("password2" in request.args):
+        password = request.args["password"]
+        password2 = request.args["password2"]
+        username = request.args["username"]
+        if (password == password2):
+            dbFunctions.addUser(username,password)
+            return redirect(url_for("success"))
+    return render_template('signup.html')
 
 @app.route("/loggedIn")
 def success():
     return render_template(
         "loggedIn.html"
         )
-
-
 
 
 db.commit() #save changes
---
+++ Team/utl/dbFunctions.py
@@ -0,0 +1,52 @@
+import sqlite3
+import csv
+
+def accountExists(username, password):
+    DB_FILE= "accounts.db"
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    command = "SELECT id, username FROM USERNAMES WHERE username = \"{}\" AND password = \"{}\";".format(username, password)
+    c.execute(command)
+    q = c.fetchall()
+    db.commit() #save changes
+    db.close()  #close database
+    if len(q) == 0:
+        return -1 #return -1 if it doesn't exist
+    else:
+        return q[0][0] #return ID of user if exists
+
+
+def userExists(username):
+    DB_FILE="accounts.db"
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    command = "SELECT id, username FROM USERNAMES WHERE username = \"{}\";".format(username)
+    c.execute(command)
+    q = c.fetchall()
+    db.commit() #save changes
+    db.close()  #close database
+    if len(q) == 0:
+        return -1 #return -1 if it doesn't exist
+    else:
+        return q[0][0] #return ID of user if exists
+
+def addUser(username, password):
+    DB_FILE="accounts.db"
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    command = "SELECT id, username FROM USERNAMES WHERE username = \"{}\";".format(username)
+    c.execute(command)
+    new = c.fetchall()
+    if len(new) == 0:
+        command = "SELECT id FROM USERNAMES;"
+        c.execute(command)
+        q = c.fetchall()
+        command = "INSERT INTO USERNAMES VALUES({}, \"{}\", \"{}\");".format(q[len(q)-1][0]+1,username,password)
+        c.execute(command)
+        db.commit() #save changes
+        db.close()  #close database
+        return True
+    else:
+        db.commit() #save changes
+        db.close()  #close database
+        return False
