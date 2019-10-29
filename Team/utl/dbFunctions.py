574f625c848ade002aebf2a0dc6d65467b8a87e9
Taejoon Kim <tkim00@stuy.edu>
2019-10-29 01:45:33 -0400

    more adjustments

--- Team/utl/dbFunctions.py
+++ Team/utl/dbFunctions.py
@@ -182,8 +182,7 @@ def recent(userID): #the list it returns should be in order
         for row in new:
             if storyID[0] == row[0]:
                 oldtext = row[2]
-        #if (canAdd(userID, storyID) == True):
-        if (True == True):
+        if (canAdd(userID, storyID) == True):
             l.append(oldtext)
         oldtext = ""
     db.commit() #save changes
