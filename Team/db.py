10ade8fec6178268c19899fcb53ca20875c9237a
Nahi.k <47545708+NahiKhan@users.noreply.github.com>
2019-10-28 20:07:36 -0400

    Update db.py

--- Team/db.py
+++ Team/db.py
@@ -6,13 +6,16 @@ c = db.cursor()
 #c = sqlite3.connect('accounts.db', check_same_thread=False).cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS USERNAMES(
             [userID] INTEGER PRIMARY KEY, [username] text NOT NULL, [password] text);''')
+           # Creates table if usernames do not exist
 c.execute('''CREATE TABLE IF NOT EXISTS STORIES
              ([storyID] INTEGER PRIMARY KEY, [title] text NOT NULL);''')
+           # Creates table if stories do not exist
 c.execute('''CREATE TABLE IF NOT EXISTS STORYEDITS
              ([storyID] INTEGER, [userID] INTEGER, [text] text NOT NULL);''')
-c.execute('''INSERT INTO STORIES VALUES(0, "title");''')
-c.execute('''INSERT INTO USERNAMES VALUES(0, "username", "password");''')
-c.execute('''INSERT INTO STORYEDITS VALUES(0, 0, "text");''')
+           # Creates table if storyedits do not exist
+c.execute('''INSERT INTO STORIES VALUES(0, "title");''') # Story values stored in table
+c.execute('''INSERT INTO USERNAMES VALUES(0, "username", "password");''') # Username values stored in a table
+c.execute('''INSERT INTO STORYEDITS VALUES(0, 0, "text");''') #Storyedit values stored in a table
 
 
 db.commit() #save changes
